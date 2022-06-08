8f245c88b4f37ed5788bbc251f59a2bc18480262
roma_marshall <roma_marshall@outlook.com>
2022-06-08 17:57:47 +0200

    rss output

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
+$RSS_URL = 'https://dev98.de/feed/';
+$feeds = simplexml_load_file($RSS_URL);
 
-$url = 'https://dev98.de/feed/';
+if ($feeds) {
+    $webpage_title = $feeds->channel->title;
+    $webpage_url = $feeds->channel->link;
 
-$feeds = simplexml_load_file($url);
+    foreach ($feeds->channel->item as $item) {
+        $title = $item->title;
+        $description = $item->description;
+        $link = $item->link;
+        $date = date('D, d M Y', strtotime($item->pubDate));
+        ?>
 
-var_dump($feeds);
+        <div class="App">
+            <h2>
+                <a href="<?= $link; ?>" target="_blank"><?= $title; ?></a>
+            </h2>
+            <p><?= $date; ?></p>
+            <p><?= $description; ?></p>
+        </div>
+
+        <?php
+    }
+}
+
+
+?>
+
+<!--<div class="main">-->
+<!--    <h1>RSS --><? // //= $webpage_title ?><!--</h1><a href='--><? // //= $webpage_url ?><!--' target='_blank'>--><? // //= $webpage_url ?><!--</a>-->
+<!--</div>-->
